c9a7f580ee64bfc22764b8f5ccec8af0e3da5a48
Teana Zhong <Teanawk@gmail.com>
2022-09-15 11:03:35 -0400

    explained code

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=cbb53a38daad4a38723d
         movies.push(movie.results[i])
         renderMovies(movie.results[i]);
     }
-
 });
 
 
-// second page of json data, newlt released movies 
+// second page of json data, newly released movies 
 fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=cbb53a38daad4a38723daab094adb724')
 .then ((res) => res.json())
 .then((movie) =>  {
@@ -21,7 +20,6 @@ fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=cbb53a38daad4a3872
         movies.push(movie.results[i])
         renderMovies(movie.results[i]);
     }
-
 });
 
 //makes the movie images load 
@@ -30,7 +28,8 @@ function renderMovies(movie) {
     let image = document.createElement("img")
     image.src = `https://image.tmdb.org/t/p/w185/${movie.poster_path}`
     panel.append(image)
-
+// adding click events so movie info will load when clicked 
+//EVENT LISTENER ONE 
 image.addEventListener("click", () => {
     console.log("clicked movie")
     renderDetails(movie)
@@ -39,13 +38,14 @@ image.addEventListener("click", () => {
     //lets you type after you click on the image without clicking search bar again
     searchBar.focus()
 })
+//when we mouse over the image it will glow
+//EVENT LISTENER 2
 image.addEventListener("mouseover" , () => {
     image.style.filter = "grayscale(0%)"
 })
+//same as above 
 image.addEventListener("mouseout" , () => {
     image.style.filter = "grayscale(100%)"
-
-
 })
 }
 
@@ -70,19 +70,25 @@ image.src = image.src = `https://image.tmdb.org/t/p/w185/${movie.poster_path}`
 
 //button that toggle night and light mode 
 const checkbox = document.getElementById("checkbox");
+//EVENT LISTENER 3
 checkbox.addEventListener("change", () =>{
     //change theme of website
     document.body.classList.toggle("dark")
 })
 
-
+//create the search bar 
 const searchBar = document.querySelector('#myInput')
 const movieList = document.querySelector('.genreButton')
 console.log(searchBar)
+//add event listener to thr search bar
+// EVENT LISTENER EXTRA 
 searchBar.addEventListener('input', (e) => {
     console.log('search', e.target.value)
+
     movieList.innerHTML = ''
+// loops through each movie and filters 
     movies.forEach(movie => {
+// the to lower case things prevents the search bar from being case sensative.
         if (movie.title.toLowerCase().includes(e.target.value.toLowerCase()) ) {
             renderMovies(movie)
         }
